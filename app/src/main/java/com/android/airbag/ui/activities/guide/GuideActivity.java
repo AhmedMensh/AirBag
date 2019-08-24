90ef6e748126822eb8940f63dae792a45ca76f84
ahmed <ahmedmensh8@gmail.com>
2019-08-24 19:01:20 +0200

    logout dilog

---
+++ app/src/main/java/com/android/airbag/ui/activities/guide/GuideActivity.java
@@ -0,0 +1,16 @@
+package com.android.airbag.ui.activities.guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.airbag.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
