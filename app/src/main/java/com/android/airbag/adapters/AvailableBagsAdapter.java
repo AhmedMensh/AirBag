d461ab74103b56e57838516eb5f2c115b963df3e
ahmed <ahmedmensh8@gmail.com>
2019-10-15 23:10:47 +0200

    create item for sendr

--- app/src/main/java/com/android/airbag/adapters/AvailableBagsAdapter.java
+++ app/src/main/java/com/android/airbag/adapters/AvailableBagsAdapter.java
@@ -1,25 +1,26 @@
 package com.android.airbag.adapters;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.airbag.R;
-import com.android.airbag.ui.fragments.available_bags.AvailableBagsFragment;
 
 public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdapter.ViewHolder> {
 
     private static final String TAG = "AvailableBagsAdapter";
     private ItemClickListener listener;
-    public interface ItemClickListener{
+
+    public interface ItemClickListener {
         void onItemClickListener(int itemPosition);
     }
 
@@ -32,7 +33,7 @@ public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdap
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bag_item, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.bag_item,parent, false);
         return new ViewHolder(view);
     }
 
@@ -48,35 +49,52 @@ public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdap
 
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        LinearLayout moreDetailsLayout;
+        LinearLayout moreDetailsLayout,bagInfoLayout;
+        Button addNewItemBtn;
+        ConstraintLayout itemInfoCL;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             moreDetailsLayout = itemView.findViewById(R.id.more_details_layout);
-            itemView.setOnClickListener(this::onClick);
+            addNewItemBtn = itemView.findViewById(R.id.add_item_btn);
+            itemInfoCL = itemView.findViewById(R.id.item_info_layout);
+            bagInfoLayout = itemView.findViewById(R.id.bag_info_layout);
+            itemView.setOnClickListener(this);
+            moreDetailsLayout.setOnClickListener(this);
+            addNewItemBtn.setOnClickListener(this);
+            bagInfoLayout.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
 
-            if (view == itemView) {
-                if (moreDetailsLayout.getVisibility() == View.GONE){
-                    expand(moreDetailsLayout);
+            switch (view.getId()) {
+                case R.id.add_item_btn:
+                    expand(itemInfoCL);
+                    addNewItemBtn.setVisibility(View.GONE);
+                    break;
+
+                case R.id.bag_info_layout:
+                    if (moreDetailsLayout.getVisibility() == View.GONE) {
+                        expand(moreDetailsLayout);
+                        addNewItemBtn.setVisibility(View.VISIBLE);
 
 //                    listener.onBagItemClickListener(getAdapterPosition());
-                }
+                    } else {
+                        collapse(moreDetailsLayout);
+                        addNewItemBtn.setVisibility(View.GONE);
+                        itemInfoCL.setVisibility(View.GONE);
 
-                else{
-
-                    collapse(moreDetailsLayout);
-
-                }
+                    }
 
             }
 
+
         }
+
     }
+
 
     public static void expand(final View v) {
         int matchParentMeasureSpec = View.MeasureSpec.makeMeasureSpec(((View) v.getParent()).getWidth(), View.MeasureSpec.EXACTLY);
@@ -87,13 +105,12 @@ public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdap
         // Older versions of android (pre API 21) cancel animations for views with a height of 0.
         v.getLayoutParams().height = 1;
         v.setVisibility(View.VISIBLE);
-        Animation a = new Animation()
-        {
+        Animation a = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
                 v.getLayoutParams().height = interpolatedTime == 1
                         ? LinearLayout.LayoutParams.WRAP_CONTENT
-                        : (int)(targetHeight * interpolatedTime);
+                        : (int) (targetHeight * interpolatedTime);
                 v.requestLayout();
             }
 
@@ -104,21 +121,20 @@ public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdap
         };
 
         // Expansion speed of 1dp/ms
-        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        a.setDuration((int) (targetHeight / v.getContext().getResources().getDisplayMetrics().density));
         v.startAnimation(a);
     }
 
     public static void collapse(final View v) {
         final int initialHeight = v.getMeasuredHeight();
 
-        Animation a = new Animation()
-        {
+        Animation a = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if(interpolatedTime == 1){
+                if (interpolatedTime == 1) {
                     v.setVisibility(View.GONE);
-                }else{
-                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                } else {
+                    v.getLayoutParams().height = initialHeight - (int) (initialHeight * interpolatedTime);
                     v.requestLayout();
                 }
             }
@@ -130,7 +146,7 @@ public class AvailableBagsAdapter extends RecyclerView.Adapter<AvailableBagsAdap
         };
 
         // Collapse speed of 1dp/ms
-        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        a.setDuration((int) (initialHeight / v.getContext().getResources().getDisplayMetrics().density));
         v.startAnimation(a);
     }
 }
