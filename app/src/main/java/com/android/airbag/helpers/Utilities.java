9bd6ffa48981283cb13b9fdb1b408add4e13abf9
ahmed <ahmedmensh8@gmail.com>
2019-10-18 00:00:07 +0200

    creat bag

--- app/src/main/java/com/android/airbag/helpers/Utilities.java
+++ app/src/main/java/com/android/airbag/helpers/Utilities.java
@@ -7,6 +7,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.airbag.R;
 import com.android.airbag.ui.activities.login.LoginActivity;
@@ -21,6 +22,7 @@ public class Utilities {
 
         View header = navigationView.getHeaderView(0);
         Button headerBtn = header.findViewById(R.id.user_type_btn);
+        TextView userTypeTv= header.findViewById(R.id.user_type_tv);
 
         int userType = SharedPreferencesManager.getIntValue(context,Constants.USER_TYPE);
 
@@ -29,7 +31,9 @@ public class Utilities {
             nav_Menu.findItem(R.id.navigation_promo_code).setVisible(false);
             headerBtn.setText("Be Sender");
             headerBtn.setBackgroundResource(R.drawable.button_background_2);
+            userTypeTv.setText("I am Carrier");
         }else {
+            userTypeTv.setText("I am Sender");
             headerBtn.setText("Be Carrier");
             headerBtn.setBackgroundResource(R.drawable.button_background_2);
         }
--- app/src/main/java/com/android/airbag/ui/fragments/pending_items/PendingItemsFragment.java
+++ app/src/main/java/com/android/airbag/ui/fragments/pending_items/PendingItemsFragment.java
@@ -6,20 +6,32 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.android.airbag.R;
+import com.android.airbag.helpers.Constants;
+import com.android.airbag.helpers.SharedPreferencesManager;
 import com.android.airbag.ui.activities.bags_list.BagListActivity;
 import com.android.airbag.ui.activities.create_bag.CreateBagActivity;
 import com.android.airbag.ui.activities.create_item.CreateItemActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PendingItemsFragment extends Fragment {
+public class PendingItemsFragment extends Fragment implements View.OnClickListener{
 
+    private static final String TAG = "PendingItemsFragment";
+    private Unbinder unbinder;
+    @BindView(R.id.new_item_btn)
+    Button addNewItemButton;
 
     public PendingItemsFragment() {
         // Required empty public constructor
@@ -32,13 +44,23 @@ public class PendingItemsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pending_items, container, false);
 
-        view.findViewById(R.id.new_item_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getContext(), CreateItemActivity.class));
-            }
-        });
+        unbinder = ButterKnife.bind(this,view);
+
+        if (SharedPreferencesManager.getIntValue(getContext(), Constants.USER_TYPE) == 1){
+            addNewItemButton.setVisibility(View.INVISIBLE);
+        }
+        addNewItemButton.setOnClickListener(view1 ->
+                startActivity(new Intent(getContext(), CreateItemActivity.class)));
         return  view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add_item_btn:
+                startActivity(new Intent(getContext(), CreateItemActivity.class));
+                Log.e(TAG, "onClick: ");
+                break;
+        }
+    }
 }
